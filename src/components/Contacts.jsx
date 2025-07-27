1627feae796a65ff03b344272beb6b79440aef28
Anish Kumar <anishkumar3967@gmail.com>
2025-07-27 10:43:24 +0530

    fix: set base path for GitHub Pages

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -6,15 +6,15 @@ function Contacts() {
     <div className="center-content">
       <div className="contact-item">
         <div className="contacts">
-        <img src="/img/logo/phone.png" className="contact-icon" alt="Phone"/>
+          <img src="./img/logo/phone.png" className="contact-icon" alt="Phone" />
         </div>
         <div className="contact">+91-8210728491</div>
       </div>
-      
+
       <div className="contact-item">
         <hr style={{ border: "1px solid ", margin: "20px 0px" }} />
         <div className="contacts mail">
-       <img src="/img/logo/mail.png" className="contact-icon" alt="Mail" />
+        <img src="./img/logo/mail.png" className="contact-icon" alt="Mail" />
         </div>
         <div className="contact">anishkumar3967@gmail.com</div>
       </div>
